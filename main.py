f363b351fbddd2cfabe57f9d939ba0517d3a490e
cardoso <gabrielmtg2@gmail.com>
2025-03-27 13:38:39 -0300

    typos

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-import yt_dlp # lib que permite comunicação e transferencia de dados com youtube 
-import subprocess # lib que permite rodar comandos cmd/powershell via codigo
-import validators #lib para validar se a url é valida (não é obrigatório mas é recomendado)
+
+import yt_dlp          # lib que permite comunicação e transferencia de dados com youtube
+import subprocess      # lib que permite rodar comandos cmd/powershell via codigo
+import validators      #lib para validar se a url é valida (não é obrigatório mas é recomendado)
 
 
 
 while True:
-    url = input("Enter a valid URL: ")
-    comando_yt_audio = ["yt-dlp","-f", "bestaudio", url] # comando que vai rodar no cmd para baixar o audio
+    url = input("Enter a valid URL: ")                                                      #input de url pelo usuário
+    comando_yt_audio = ["yt-dlp","-f", "bestaudio", url]                                    #comando cmd para download
     
-    if validators.url(url):
-        processo = subprocess.run(comando_yt_audio, capture_output=True, text=True)
-        print(processo)
+    if validators.url(url):                                                                 #validaçãop de url
+        processo = subprocess.run(comando_yt_audio, capture_output=True, text=True)         #guardando output do comando cmd
+        print(processo)                                                                     #printando output do comando cmd
         break
     else:
-        print("Invalid URL. Please try again.")
+        print("Invalid URL. Please try again.") 
