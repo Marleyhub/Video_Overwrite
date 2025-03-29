7e4ac06ec1614df9375c0b8a33cf57235ffdcd58
cardoso <gabrielmtg2@gmail.com>
2025-03-29 12:41:06 -0300

    coments

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
 
 import yt_dlp          # lib que permite comunicação e transferencia de dados com youtube
 import subprocess      # lib que permite rodar comandos cmd/powershell via codigo
-import validators      #lib para validar se a url é valida (não é obrigatório mas é recomendado)
-import glob
-import os
+import validators      # lib para validar se a url é valida (não é obrigatório mas é recomendado)
+import glob            # lib para achar arquivos a partir da sua extensão
+import os              # lib para interagir com arquivos
 
 
 
 while True:
-    url = input("Enter a valid URL: ")                                                      #input de url pelo usuário                                                                               
+    url = input("Enter a valid URL: ")                                                      # input de url pelo usuário                                                                               
     output_name = input("Enter a name for the audio: ")
     output_template = f"{output_name}.%(ext)s"
-    comando_yt_audio = ["yt-dlp","-f", "bestaudio", "-o", output_template, url]             #comando cmd para download  
+    comando_yt_audio = ["yt-dlp","-f", "bestaudio", "-o", output_template, url]             # comando cmd para download  
 
-    if validators.url(url):                                                                 #validaçãop de url
-        processo = subprocess.run(comando_yt_audio, capture_output=True, text=True)         #guardando output do comando cmd
-        downloaded_files = glob.glob(f"{output_name}.*")
+    if validators.url(url):                                                                 # validaçãop de url
+        processo = subprocess.run(comando_yt_audio, capture_output=True, text=True)         # guardando output do comando cmd
+        downloaded_files = glob.glob(f"{output_name}.*")                                    # colocando o arquivo baixado na viariável 
         
         if not downloaded_files: 
             print("Fail: Couldn't find this audio")
             continue
 
-        print(processo)                                                                     #printando output do comando cmd
+        print(processo)                                                                     # printando output do comando cmd
 
         audio_input = downloaded_files[0]
         audio_output = f"converted_{output_name}.mp3"
 
-        #convertendo audio 
-        comando_ffmpeg = ["ffmpeg", "-i", audio_input, "-vn", "-ab", "192k", "-y", audio_output]
-        subprocess.run(comando_ffmpeg)
+        comando_ffmpeg = ["ffmpeg", "-i", audio_input, "-vn", "-ab", "192k", "-y", audio_output]     # comando cmd para converter formatos
+        subprocess.run(comando_ffmpeg)  
 
         print(f"Áudio convertido com sucesso: {audio_output}")
 
