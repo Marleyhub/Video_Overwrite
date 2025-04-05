4b067a1a65e2875626e36453a99e94cb59216bde
cardoso <cardoso.gab@outlook.com>
2025-04-05 20:09:50 -0300

    Editando comentários e adicionando whisper

--- main.py
+++ main.py
@@ -4,31 +4,38 @@ import subprocess      # lib que permite rodar comandos cmd/powershell via codig
 import validators      # lib para validar se a url é valida (não é obrigatório mas é recomendado)
 import glob            # lib para achar arquivos a partir da sua extensão
 import os              # lib para interagir com arquivos
+import whisper         # lib para transcrever audio // Obs: ultilizar python 3.10.9
 
 
 
 while True:
-    url = input("Enter a valid URL: ")                                                      # input de url pelo usuário                                                                               
+
+    url = input("Enter a valid URL: ")                                                              # input de url pelo usuário                                                                               
     output_name = input("Enter a name for the audio: ")
     output_template = f"{output_name}.%(ext)s"
-    comando_yt_audio = ["yt-dlp","-f", "bestaudio", "-o", output_template, url]             # comando cmd para download  
-
-    if validators.url(url):                                                                 # validaçãop de url
-        processo = subprocess.run(comando_yt_audio, capture_output=True, text=True)         # guardando output do comando cmd
-        downloaded_files = glob.glob(f"{output_name}.*")                                    # colocando o arquivo baixado na viariável 
+    comando_yt_audio = ["yt-dlp","-f", "bestaudio", "-o", output_template, url]                     # comando cmd para download  
+  
+    if validators.url(url):                                                                         # validaçãop de url
+        processo = subprocess.run(comando_yt_audio, capture_output=True, text=True)                 # guardando output do comando cmd
+        downloaded_files = glob.glob(f"{output_name}.*")                                            # colocando o arquivo baixado na viariável 
         
         if not downloaded_files: 
             print("Fail: Couldn't find this audio")
             continue
-        print(processo)                                                                     # printando output do comando cmd
+        print(processo)                                                                             # printando output do comando cmd
 
         audio_input = downloaded_files[0]
         audio_output = f"converted_{output_name}.mp3"
-        comando_ffmpeg = ["ffmpeg", "-i", audio_input, "-vn", "-ab", "192k", "-y", audio_output]     # comando cmd para converter formatos
+        comando_ffmpeg = ["ffmpeg", "-i", audio_input, "-vn", "-ab", "192k", "-y", audio_output]    # comando cmd para converter formatos
         subprocess.run(comando_ffmpeg)  
         print(f"Áudio convertido com sucesso: {audio_output}")
 
         os.remove(audio_input)
+
+        #whisper config
+        model = whisper.load_model("base")
+        result = model.transcribe(f"./{audio_output}")
+        print(f"The output text is: \n {result['text']}")
         break
 
     else:
