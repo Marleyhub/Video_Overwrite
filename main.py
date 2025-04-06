ff4d8635573a816b91e34082bf5ed2ffb8328634
cardoso <cardoso.gab@outlook.com>
2025-04-05 22:02:57 -0300

    configuração do gpt4all

--- main.py
+++ main.py
@@ -34,14 +34,21 @@ while True:
         os.remove(audio_input)
 
         #whisper config
-        model = whisper.load_model("base")
-        result = model.transcribe(f"./{audio_output}")
+        whisper_model = whisper.load_model("base")
+        result = whisper_model.transcribe(f"./{audio_output}")
         print(f"The output text is: \n {result['text']}")
+
+        #gpt4all config
+        gpt4all_model = GPT4All('ggml-gpt4all-j-v1.3-groovy.bin')
+        audio_data = result['text']
+        prompt = f"Crie um resumo organizado em tópicos com um índece e um título adequado desse texto '{audio_data}'"
+        gpt4all_output = gpt4all_model.generate(prompt)
+        print(f'The orgnized text of the audio: \n {gpt4all_output}')
         break
 
     else:
         print("Invalid URL. Please try again.") 
 
 
-        # Usar a AI whisper para transcrição que funciona localmente e é simples de usar
-        # Após a transcrição usamos qa gemini para resumir e organizar a transcrição git c
+        
+       
