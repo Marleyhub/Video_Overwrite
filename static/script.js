e2d1ec0c1d96461b6ca3d75ef11b2bc83806f2c7
cardoso <cardoso.gab@outlook.com>
2025-05-05 15:53:00 -0300

    commentig front-end

--- static/script.js
+++ static/script.js
@@ -1,19 +1,25 @@
+
+// Tratamento de url e audio
 async function processAudio() {
+
+    //loading
     document.getElementById("loading").classList.remove("hidden");
     document.getElementById("output").innerText = "";
-
+    //url data
     const url = document.getElementById("url").value;
     const name = document.getElementById("name").value;
 
+    //Process Audio route
     const response = await fetch("http://localhost:5000/process", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ url, name })
     });
-
     const data = await response.json();
+    
     document.getElementById("loading").classList.add("hidden");
 
+    //output
     if (data.error) {
       document.getElementById("output").innerText = "❌ " + data.error;
     } else {
